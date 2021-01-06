3565ac442059468b0028b0d2b11d7e25764d8b35
prakashperam <prakashperam123@gmail.com>
2021-01-06 19:22:21 +0530

    filtered single room with in context and updated on room component

--- src/context.js
+++ src/context.js
@@ -1,15 +1,51 @@
 import React, { Component } from "react";
+import items from "./data";
 
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
   state = {
-    greeting: "hello",
-    name: "prakash",
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true,
   };
+  //getData
+
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    // console.log(rooms, "rooms in contenxt");
+    let featuredRooms = rooms.filter((room) => room.featured === true);
+    // console.log(featuredRooms, "in context");
+    this.setState({
+      rooms,
+      featuredRooms,
+      sortedRooms: rooms,
+      loading: false,
+    });
+  }
+
+  formatData(items) {
+    let tempItems = items.map((item) => {
+      let id = item.sys.id;
+      let images = item.fields.images.map((image) => {
+        return image.fields.file.url;
+      });
+      let room = { ...item.fields, images, id };
+      return room;
+    });
+    return tempItems;
+  }
+
+  getRoom = (slug) => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find((room) => room.slug === slug);
+    return room;
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
